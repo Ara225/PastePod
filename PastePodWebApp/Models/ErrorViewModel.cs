e10ee320f562c5f64dd7d583ca8b1f60915b2d75
Anna Aitchison <anna11199810@hotmail.co.uk>
2021-01-24 21:14:09 +0000

    Add project files.

---
+++ PastePodWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PastePodWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
