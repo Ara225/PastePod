9af374ad943bb49097b58c0bfb138d60e1ada0c6
Anna Aitchison <anna11199810@hotmail.co.uk>
2021-01-25 23:59:10 +0000

    Implement the controller side changes to support loading and saving

--- PastePodWebApp/Controllers/HomeController.cs
+++ PastePodWebApp/Controllers/HomeController.cs
@@ -6,21 +6,44 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using PastePodWebApp.Data;
 
 namespace PastePodWebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly TextDocumentDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, TextDocumentDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            if (Request.Path.HasValue)
+            {
+                string[] path = Request.Path.Value.Split("/");
+                string fileName = path[path.Count() - 1];
+                Guid test;
+                if (fileName != null && Guid.TryParse(fileName, out test)) 
+                {
+                    Debug.WriteLine(fileName);
+                    string fileContent = await DataAccess.GetDocument(fileName);
+                    Debug.WriteLine(fileContent);
+                    ViewBag.DocumentContent = fileContent;
+                }
+            }
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(TextDocumentViewModel model)
+        {
+            string fileName = await DataAccess.SaveDocument(model, _context);
+            return Redirect("/Home/Index/" + fileName);
         }
 
         public IActionResult Privacy()
