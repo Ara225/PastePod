d4bebd6fae71d2b7d1a4247aa12366587bcc18c8
Anna Aitchison <anna11199810@hotmail.co.uk>
2021-02-03 22:16:11 +0000

    Scaffold identity UI

---
+++ PastePodWebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PastePodWebApp.Data;
+
+[assembly: HostingStartup(typeof(PastePodWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace PastePodWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
